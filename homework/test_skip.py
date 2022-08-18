21ecab9381ef8d6421bf976c38dd9b8b85ebbec6
Мокрецова Полина Максимовна <mokretsova@tochka.com>
2022-08-18 18:15:27 +0500

    edit test_skip

--- homework/test_skip.py
+++ homework/test_skip.py
@@ -5,30 +5,31 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='function')
-def browser_desktop():
-    browser.config.browser_name = 'chrome'
-    browser.config.window_width = 1200
-    browser.config._window_height = 1100
+@pytest.fixture(params=[(1920, 1080), (1024, 768), (420, 768), (320, 768)])
+def browser_size(request):
+    return request
 
 
-@pytest.fixture(scope='function')
-def browser_mobile():
-    browser.config.browser_name = 'chrome'
-    browser.config.window_width = 310
-    browser.config._window_height = 516
+@pytest.fixture(scope='function', autouse=True)
+def browser_config(browser_size):
+    browser.open('https://github.com/')
+    width = browser_size.param[0]
+    height = browser_size.param[1]
+    browser.driver.set_window_size(width=width, height=height)
 
 
-def test_github_desktop(browser_mobile):
-    if browser.config.window_width < 1000:
+def test_github_desktop(browser_size):
+    if browser.driver.get_window_size()["width"] < 1010:
         pytest.skip('Size for mobile version')
     browser.open('https://github.com/')
     browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
 
 
-def test_github_mobile(browser_desktop):
-    if browser.config.window_height > 1000:
-        pytest.skip('Size for browser version')
+def test_github_mobile():
+    if browser.driver.get_window_size()["width"] > 1011:
+        pytest.skip('Size for desktop version')
     browser.open('https://github.com/')
     browser.element('[class="octicon octicon-three-bars"]').click()
     browser.element('[class="HeaderMenu-link flex-shrink-0 no-underline"]').click()
+
+
